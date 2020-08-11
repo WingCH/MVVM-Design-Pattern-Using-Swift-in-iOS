e5fcec9c606ff51691ebb3e49a5fefaaef5f526c
WingCH <wingchan@vgt-hk.net>
2020-08-11 17:31:03 +0800

    update 12 5:34

--- setting-up-user-interface/GoodNews/GoodNews/Controllers/NewsListTableViewController.swift
+++ setting-up-user-interface/GoodNews/GoodNews/Controllers/NewsListTableViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 
 class NewsListTableViewController: UITableViewController {
 
+    private var articleListVM: ArticleListViewModel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
@@ -22,19 +24,34 @@ class NewsListTableViewController: UITableViewController {
 
         let url = URL(string: "https://newsapi.org/v2/top-headlines?apiKey=a0619c76d3084717bc466b69ea59a7c6&country=hk&q=蘋果".urlEncoded())!
 
-        Webservice().getArticles(url: url) { _ in
+        Webservice().getArticles(url: url) { articles in
+            if let articles = articles {
+                self.articleListVM = ArticleListViewModel(articles: articles);
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+        }
+    }
 
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return self.articleListVM == nil ? 0 : self.articleListVM.numberOfSections
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.articleListVM.numberOfRowInSection(section)
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleTableViewCell", for: indexPath) as? ArticleTableViewCell else {
+            fatalError("ArticleTableViewCell not found")
         }
 
+        let articleVM = self.articleListVM.articleAtIndex(indexPath.row)
+
+        cell.titleLabel.text = articleVM.title
+        cell.descriptionLabel.text = articleVM.description
+        return cell
     }
-
-
-    func test(value: String) {
-        print(value);
-    }
-
-    func aaa(_ value: String) {
-        print(value);
-    }
-
 }
--- setting-up-user-interface/GoodNews/GoodNews/View Models/ArticleViewModel.swift
+++ setting-up-user-interface/GoodNews/GoodNews/View Models/ArticleViewModel.swift
@@ -5,8 +5,28 @@
 
 import Foundation
 
+struct ArticleListViewModel {
+    let articles: [Article]
+}
+
+extension ArticleListViewModel {
+    var numberOfSections: Int {
+        return 1;
+    }
+
+    func numberOfRowInSection(_ section: Int) -> Int {
+        return self.articles.count
+    }
+
+    func articleAtIndex(_ index: Int) -> ArticleViewModel {
+        let article = self.articles[index]
+        return ArticleViewModel(article)
+    }
+
+}
+
 struct ArticleViewModel {
-    private let article: String
+    private let article: Article
 
     // 預設init
 //    init(article: String) {
@@ -20,7 +40,17 @@ extension ArticleViewModel {
     // 預設會生成init 如上 ArticleViewModel.init(article: "AAA")
     // 如果用左"_" 只需要ArticleViewModel.init("AAA")
     // 因為呢個特性 所以無撞到
-    init(_ article: String) {
+    init(_ article: Article) {
         self.article = article;
+    }
+}
+
+extension ArticleViewModel {
+    var title: String {
+        self.article.title ?? "no title";
+    }
+
+    var description: String {
+        self.article.description ?? "no description";
     }
 }
