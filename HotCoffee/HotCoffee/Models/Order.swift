bdbdfe8a90dcf8cd9581c2ac5158d3b956a2423e
WingCH <wingchan@vgt-hk.net>
2020-08-12 15:18:59 +0800

    add comment

--- HotCoffee/HotCoffee/Models/Order.swift
+++ HotCoffee/HotCoffee/Models/Order.swift
@@ -35,6 +35,7 @@ struct Order: Codable {
     let size: CoffeeSize?
 }
 
+// handle enum unknown case
 // https://zhuanlan.zhihu.com/p/83816429
 protocol CodableEnumeration: RawRepresentable, Codable where RawValue: Codable {
     static var defaultCase: Self { get }
