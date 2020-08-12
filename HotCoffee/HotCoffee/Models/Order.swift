c39b58023b16d1c6d07c6dba2f0dde320b791d66
WingCH <wingchan@vgt-hk.net>
2020-08-12 11:39:58 +0800

    done 18

---
+++ HotCoffee/HotCoffee/Models/Order.swift
@@ -0,0 +1,26 @@
+//
+// Created by WingCH on 12/8/2020.
+// Copyright (c) 2020 Mohammad Azam. All rights reserved.
+//
+
+import Foundation
+
+enum CoffeeType: String, Codable {
+    case cappuccino
+    case latte
+    case espressino
+    case cortado
+}
+
+enum CoffeeSize: String, Codable {
+    case small
+    case medium
+    case large
+}
+
+struct Order: Codable {
+    let name: String
+    let email: String
+    let type: CoffeeType
+    let size: CoffeeSize
+}
