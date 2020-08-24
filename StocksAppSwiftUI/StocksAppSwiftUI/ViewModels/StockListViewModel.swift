9fc32fb1dfeab1483dd922c1dce879b7bc8b0af7
WingCH <wingchan@vgt-hk.net>
2020-08-24 16:33:29 +0800

    done 36

---
+++ StocksAppSwiftUI/StocksAppSwiftUI/ViewModels/StockListViewModel.swift
@@ -0,0 +1,11 @@
+//
+// Created by WingCH on 24/8/2020.
+// Copyright (c) 2020 WingCH. All rights reserved.
+//
+
+import Foundation
+
+class StockListViewModel {
+    var searchTerm: String = ""
+    var stocks: [StockViewModel] = [StockViewModel]()
+}
